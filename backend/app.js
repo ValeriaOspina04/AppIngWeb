4ab03da9e17c936d1bb8175c73d0a04cbd9c64b7
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-15 09:17:08 -0500

    puerto

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ app.use('/api/controles', controlRoutes);
 //app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Backend funcionando en Vercel 🚀");
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
 
 module.exports = app;
