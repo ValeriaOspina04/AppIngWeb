ec027fe8b205052c131952cd4fe60323bfae0303
Valeria04 <vale-ospina2005@outlook.com>
2026-04-16 18:47:24 -0500

    conectar frontend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,16 +7,22 @@ const authRoutes = require('./routes/authRoutes'); // Para login/registro
 
 const app = express();
 
+// servir archivos estáticos (frontend)
+app.use(express.static(path.join(__dirname, "../frontend")));
 app.use(cors());
 app.use(express.json()); // Protección básica contra inyecciones y manejo de JSON
 
 // Rutas
 app.use('/api/controles', controlRoutes);
-//app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-  res.send("Backend funcionando en Vercel 🚀");
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
 
 module.exports = app;
