5f7fefa2602ca0205428cd529e3344eb46e5f4f3
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-22 11:02:53 -0500

    Change routes to remove .html extensions

--- backend/app.js
+++ backend/app.js
@@ -20,11 +20,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/pages/login.html"));
 });
 
-app.get("/registro.html", (req, res) => {
+app.get("/registro", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/pages/registro.html"));
 });
 
-app.get("/dashboard.html", (req, res) => {
+app.get("/dashboard", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend/pages/dashboard.html"));
 });
 
