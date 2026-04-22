4b3564b3173134f043ada61d270746630b59c6ec
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-22 10:56:15 -0500

    Add connection timeout to database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
+    connectTimeout: 20000,
 });
 
 module.exports = pool.promise();
