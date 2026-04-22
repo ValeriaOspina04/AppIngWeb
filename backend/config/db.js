541a7285a19991203892ac77f0f69f38b408008a
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-22 11:01:42 -0500

    variables de clever

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10
+const db = mysql.createPool({
+  host: process.env.MYSQL_ADDON_HOST,      // <--- Cambiado
+  user: process.env.MYSQL_ADDON_USER,      // <--- Cambiado
+  password: process.env.MYSQL_ADDON_PASSWORD,  // <--- Cambiado
+  database: process.env.MYSQL_ADDON_DB,    // <--- Cambiado
+  port: process.env.MYSQL_ADDON_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10
     connectTimeout: 20000,
 });
 
