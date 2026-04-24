9f81c318aa42fee6af7f35909b5705e9028cfcdf
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-24 05:37:42 -0500

    Update dashboard.js

--- backend/frontend/js/dashboard.js
+++ backend/frontend/js/dashboard.js
@@ -199,10 +199,7 @@ async function guardarProgreso() {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             },
-            body: JSON.stringify({ 
-                controles: controles, 
-                nombre_empresa: nombreEmpresa 
-            })
+            body: JSON.stringify({controles})
             
         });
 
