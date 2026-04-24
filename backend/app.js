a20bbc8853a901f7b7735261b6e0862cb8d6a6c2
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-23 19:03:51 -0500

    Refactor routes and static file serving

--- backend/app.js
+++ backend/app.js
@@ -2,30 +2,32 @@ const path = require("path");
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const controlRoutes = require('./routes/controlRoutes');
+const authRoutes = require('./routes/authRoutes'); // Para login/registro
 
 const app = express();
 
 // servir archivos estáticos (frontend)
 app.use(express.static(path.join(__dirname, "frontend")));
-app.use(express.static(path.join(__dirname, '../frontend/pages')));
 app.use(cors());
 app.use(express.json()); // Protección básica contra inyecciones y manejo de JSON
 
 // Rutas
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/controles', require('./routes/controlRoutes'));
-
+app.use('/api/controles', controlRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/login.html"));
   res.sendFile(path.join(__dirname, "frontend/pages/login.html"));
 });
 
 app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/registro.html"));
   res.sendFile(path.join(__dirname, "frontend/pages/registro.html"));
 });
 
 app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dashboard.html"));
   res.sendFile(path.join(__dirname, "frontend/pages/dashboard.html"));
 });
 
