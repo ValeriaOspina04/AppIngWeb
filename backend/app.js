201b47afe863f659bbc981312ec83d4200c9c9e4
ValeriaOspina04 <141794454+ValeriaOspina04@users.noreply.github.com>
2026-04-24 02:47:56 -0500

    Update CORS

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,11 @@ const controlRoutes = require('./routes/controlRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://appingweb.onrender.com', 
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
